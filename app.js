c704faa4d0f119d3b605eff11f997ad2b6c6e134
Lilith Nix <andromeda0209@gmail.com>
2023-03-18 20:50:29 -0300

    made a desaster

--- app.js
+++ app.js
@@ -1,16 +1,22 @@
+//<li> features and company:
 const feature = document.getElementById('features');
 const company = document.getElementById('company');
-
+//submenus:
 const featuresUl = document.getElementById('features-ul');
 const companyUl = document.getElementById('company-ul');
-
+//arrows next to the <li>:
 const iconArrowF = document.querySelector('.icon-arrowF');
 const iconArrowC = document.querySelector('.icon-arrowC');
+//hamburguer and cross icons:
+const openMenu = document.querySelector('.open-menu');
+const closeMenu = document.querySelector('.close-menu');
+//navegation:
+const nav = document.querySelector('.nav');
 
-
+//When clicking on features <li>, the submenu should open or close:
 feature.addEventListener('click', () => {
  
-    if (featuresUl.style.visibility === hidden){
+    if (featuresUl.style.visibility === 'hidden'){
         
         featuresUl.style.visibility = 'visible';
         iconArrowF.src = "./images/icon-arrow-down.svg";
@@ -23,6 +29,7 @@ feature.addEventListener('click', () => {
     }
 });
 
+//When clicking on company <li>, the submenu should open or close:
 company.addEventListener('click', () => {
     
     if (companyUl.style.visibility === 'hidden'){
@@ -36,4 +43,16 @@ company.addEventListener('click', () => {
         iconArrowC.src = "./images/icon-arrow-up.svg";
 
     }
+});
+
+openMenu.addEventListener('click', () => {
+    nav.className = 'nav-visible'; //nav becomes visible
+    closeMenu.className = 'close-menu-active'; //cross icon appears
+    openMenu.className = 'open-menu-inactive'; //hamburguer icon dessapears
+});
+
+closeMenu.addEventListener('click', () => {
+    //when cross icon is clicked, the nav desappears
+    nav.classList.remove = 'nav-visible';
+    closeMenu.classList.remove = 'close-menu-inactive';
 });
