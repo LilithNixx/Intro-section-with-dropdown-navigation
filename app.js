d2d9bd9c1a1b70dab23f107335117f391ed28274
Lilith Nix <andromeda0209@gmail.com>
2023-03-25 23:53:55 -0300

    fixed icons. Need to position correctly submenus in mobile screen and the shadow that overflows

--- app.js
+++ app.js
@@ -15,24 +15,42 @@ const nav = document.querySelector('.nav');
 
 //When clicking on features <li>, the submenu should open or close:
 feature.addEventListener('click', () => {
-
+    
+    const menuIconF = document.querySelector('.icon-arrowF');
+   
     featuresUl.classList.toggle('submenu-active');
+    companyUl.classList.remove('submenu-active');
 
-    if (featuresUl.classList === 'submenu-active') {
-
-        companyUl.classList.remove('submenu-active');
+   if (featuresUl.classList !== 'submenu-active'){
+        menuIconF.setAttribute('src', './images/icon-arrow-up.svg');
+    } 
+    
+    if (featuresUl.classList[1] === 'submenu-active') {
+        menuIconF.setAttribute('src', './images/icon-arrow-down.svg');
     }
     
 });
 
 //When clicking on company <li>, the submenu should open or close:
 company.addEventListener('click', () => {
-    
-    companyUl.classList.toggle('submenu-active');
    
+    const menuIconC = document.querySelector('.icon-arrowC');
+    //opening and closing submenu:
+    companyUl.classList.toggle('submenu-active');
+    //closing the other menu if this one is clicked:
+    featuresUl.classList.remove('submenu-active');
+    
+    if (companyUl.classList !== 'submenu-active'){
+        menuIconC.setAttribute('src', './images/icon-arrow-up.svg');
+    } 
+    
+    if (companyUl.classList[1] === 'submenu-active') {
+        menuIconC.setAttribute('src', './images/icon-arrow-down.svg');
+    }
+    
 });
 
-
+//open-close hamburguer menu:
 menuIcon.addEventListener('click', () => {
     const menuIcon2 = document.querySelector('.menu-icon i');
     const navText = document.querySelector('.nav-text');
